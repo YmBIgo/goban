049d47599805dd00a05d7b4786388a2701790265
coffeecup <IgYmB0218LP@gmail.com>
2022-04-19 13:07:18 +0900

    impl goishi block

--- src/components/Goban.tsx
+++ src/components/Goban.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useState, useRef} from "react"
 
 import "../css/goban.css"
 import black_stone from "../public/goishi_black.png"
@@ -13,7 +13,14 @@ const Goban = () => {
 
 	const [goban_state, set_goban_state] = useState<string[][]>(initial_goban)
 	const [current_phase, set_current_phase] = useState<string>("b")
-	const [goishi_block, set_goishi_block] = useState<number[][][]>([])
+	const [banmen_goishi_black, set_banmen_goishi_black] = useState<number[][]>([])
+	const [banmen_goishi_white, set_banmen_goishi_white] = useState<number[][]>([])
+	const goishi_block_black = useRef<number[][][]>([])
+	const goishi_block_white = useRef<number[][][]>([])
+
+	const current_goishi_block = useRef<number[][]>([])
+	const current_goishi_vector = useRef<number[]>([])
+	const current_goishi_vector_array = useRef<number[][]>([])
 
 	const calculate_goban_css = (i: number, j: number) => {
 		const className = ""
@@ -37,17 +44,207 @@ const Goban = () => {
 		return "goban-masu goban-center"
 	}
 
+	const initializeGoban = () => {
+		set_goban_state(initial_goban)
+		set_current_phase("b")
+		set_banmen_goishi_black([])
+		set_banmen_goishi_white([])
+		current_goishi_block.current = []
+		goishi_block_black.current = []
+		goishi_block_white.current = []
+	}
+
 	const onClickMasu = (i: number, j: number) => {
 		console.log(i, j)
-		const goban_state_copy: string[][] = goban_state.slice()
+		// goban
+		let goban_state_copy: string[][] = goban_state.slice()
+		if (goban_state_copy[i][j] != "") {
+			return
+		}
 		goban_state_copy[i][j] = current_phase
 		set_goban_state(goban_state_copy)
+
+		// banmen ishi
+		let banmen_goishi_black_copy = banmen_goishi_black.slice()
+		let banmen_goishi_white_copy = banmen_goishi_white.slice()
+
+		// banmen ishi black
+		if (current_phase == "b") {
+			banmen_goishi_black_copy.push([i, j])
+			set_banmen_goishi_black(banmen_goishi_black_copy)
+			// calculate block
+			calculate_goishi_block(banmen_goishi_black_copy, banmen_goishi_white_copy, current_phase)
+		}
+		// banmen ishi white
+		if (current_phase == "w") {
+			banmen_goishi_white_copy.push([i, j])
+			set_banmen_goishi_white(banmen_goishi_white_copy)
+			// calculate block
+			calculate_goishi_block(banmen_goishi_black_copy, banmen_goishi_white_copy, current_phase)
+		}
+
+		// calc atari
+		calc_atari()
+
+		// next move
 		const next_move = current_phase == "b" ? "w" : "b"
 		set_current_phase(next_move)
 	}
 
-	const calculate_goishi_block = () => {
-		
+	const calc_atari = () => {
+
+	}
+
+	const calculate_goishi_block = (banmen_goishi_black_copy: number[][], banmen_goishi_white_copy: number[][], current_phase: string) => {
+		let banmen_goishi_result = []
+		goishi_block_black.current = []
+		goishi_block_white.current = []
+		if(current_phase == "b") {
+			banmen_goishi_black_copy.forEach((ishi) => {
+				current_goishi_block.current = []
+				// check 
+				const is_ishi_exist = is_2dimension_array_include(goishi_block_black.current.flat(), ishi)
+				if (is_ishi_exist) {
+					return
+				}
+				// add goishi to current goishi block
+				const current_goishi_block_copy = current_goishi_block.current
+				current_goishi_block_copy.push(ishi)
+				current_goishi_block.current = current_goishi_block_copy
+				// 
+				each_calculate_goishi_block(ishi, banmen_goishi_black_copy, "")
+				console.log("final black ishi", current_goishi_block.current)
+				// 
+				const goishi_block_black_copy = goishi_block_black.current.slice()
+				goishi_block_black_copy.push(current_goishi_block.current)
+				goishi_block_black.current = goishi_block_black_copy
+			})
+		} else if (current_phase == "w") {
+			banmen_goishi_white_copy.forEach((ishi) => {
+				current_goishi_block.current = []
+				const is_ishi_exist = is_2dimension_array_include(goishi_block_white.current.flat(), ishi)
+				if (is_ishi_exist) {
+					return
+				}
+				// add goishi to current goishi block
+				const current_goishi_block_copy = current_goishi_block.current
+				current_goishi_block_copy.push(ishi)
+				current_goishi_block.current = current_goishi_block_copy
+				//
+				each_calculate_goishi_block(ishi, banmen_goishi_white_copy, "")
+				console.log("final white ishi", current_goishi_block.current)
+				//
+				const goishi_block_white_copy = goishi_block_white.current.slice()
+				goishi_block_white_copy.push(current_goishi_block.current)
+				goishi_block_white.current = goishi_block_white_copy
+			})
+		}
+	}
+
+	const each_calculate_goishi_block = (goishi: number[], banmen_goishi: number[][], houkou: string) => {
+		const filtered_current_goishi_block = current_goishi_block.current.filter((c) => {
+			if (c[0] == goishi[0] && c[1] == goishi[1]) {
+				return false
+			}
+			return true
+		})
+		if (is_2dimension_array_include(filtered_current_goishi_block, goishi)) {
+			console.log("Goishi stops @", goishi)
+			return
+		}
+		if (houkou == "") {
+			current_goishi_vector.current = [0, 0]
+			current_goishi_vector_array.current = [[0, 0]]
+		}
+
+		const left_ishi		= [goishi[0], goishi[1] - 1]
+		const right_ishi 	= [goishi[0], goishi[1] + 1]
+		const top_ishi		= [goishi[0] + 1, goishi[1]]
+		const bottom_ishi	= [goishi[0] - 1, goishi[1]]
+		const left_ishi_str		= goishi[0].toString() + (goishi[1] - 1).toString()
+		const right_ishi_str 	= goishi[0].toString() + (goishi[1] + 1).toString()
+		const top_ishi_str		= (goishi[0] + 1).toString() + goishi[1].toString()
+		const bottom_ishi_str	= (goishi[0] - 1).toString() + goishi[1].toString()
+		const banmen_goishi_flat: string[] = banmen_goishi.map((b) => b[0].toString() + b[1].toString())
+
+		if (houkou != "right" && banmen_goishi_flat.includes(left_ishi_str)) {
+			const current_goishi_block_copy = current_goishi_block.current.slice()
+			// 
+			current_goishi_vector.current = [current_goishi_vector.current[0], current_goishi_vector.current[1] - 1]
+			if (is_2dimension_array_include(current_goishi_vector_array.current, current_goishi_vector.current)) {
+				// console.log("same vector found right", current_goishi_vector_array.current, current_goishi_vector.current)
+				return
+			}
+			const current_goishi_vector_array_copy = current_goishi_vector_array.current.slice()
+			current_goishi_vector_array_copy.push(current_goishi_vector.current)
+			current_goishi_vector_array.current = current_goishi_vector_array_copy
+			// 
+			console.log("left find", left_ishi)
+			current_goishi_block_copy.push(left_ishi)
+			current_goishi_block.current = current_goishi_block_copy
+			each_calculate_goishi_block(left_ishi, banmen_goishi, "left")
+			console.log("current ishi", current_goishi_block_copy)
+		}
+		if (houkou != "left" && banmen_goishi_flat.includes(right_ishi_str)) {
+			const current_goishi_block_copy = current_goishi_block.current.slice()
+			// 
+			current_goishi_vector.current = [current_goishi_vector.current[0], current_goishi_vector.current[1] + 1]
+			if (is_2dimension_array_include(current_goishi_vector_array.current, current_goishi_vector.current)) {
+				// console.log("same vector found left", current_goishi_vector_array.current, current_goishi_vector.current)
+				return
+			}
+			const current_goishi_vector_array_copy = current_goishi_vector_array.current.slice()
+			current_goishi_vector_array_copy.push(current_goishi_vector.current)
+			current_goishi_vector_array.current = current_goishi_vector_array_copy
+			// 
+			console.log("right find", right_ishi)
+			current_goishi_block_copy.push(right_ishi)
+			current_goishi_block.current = current_goishi_block_copy
+			each_calculate_goishi_block(right_ishi, banmen_goishi, "right")
+			console.log("current ishi", current_goishi_block_copy)
+		}
+		if (houkou != "bottom" && banmen_goishi_flat.includes(top_ishi_str)) {
+			const current_goishi_block_copy = current_goishi_block.current.slice()
+			// 
+			current_goishi_vector.current = [current_goishi_vector.current[0] - 1, current_goishi_vector.current[1]]
+			if (is_2dimension_array_include(current_goishi_vector_array.current, current_goishi_vector.current)) {
+				// console.log("same vector found bottom", current_goishi_vector_array.current, current_goishi_vector.current)
+				return
+			}
+			const current_goishi_vector_array_copy = current_goishi_vector_array.current.slice()
+			current_goishi_vector_array_copy.push(current_goishi_vector.current)
+			current_goishi_vector_array.current = current_goishi_vector_array_copy
+			// 
+			console.log("top find", top_ishi)
+			current_goishi_block_copy.push(top_ishi)
+			current_goishi_block.current = current_goishi_block_copy
+			each_calculate_goishi_block(top_ishi, banmen_goishi, "top")
+			console.log("current ishi", current_goishi_block_copy)
+		}
+		if (houkou != "top" && banmen_goishi_flat.includes(bottom_ishi_str)) {
+			const current_goishi_block_copy = current_goishi_block.current.slice()
+			// 
+			current_goishi_vector.current = [current_goishi_vector.current[0] + 1, current_goishi_vector.current[1]]
+			if (is_2dimension_array_include(current_goishi_vector_array.current, current_goishi_vector.current)) {
+				// console.log("same vector found top", current_goishi_vector_array.current, current_goishi_vector.current)
+				return
+			}
+			const current_goishi_vector_array_copy = current_goishi_vector_array.current.slice()
+			current_goishi_vector_array_copy.push(current_goishi_vector.current)
+			current_goishi_vector_array.current = current_goishi_vector_array_copy
+			//
+			console.log("bottom find", bottom_ishi)
+			current_goishi_block_copy.push(bottom_ishi)
+			current_goishi_block.current = current_goishi_block_copy
+			each_calculate_goishi_block(bottom_ishi, banmen_goishi, "bottom")
+			console.log("current ishi", current_goishi_block_copy)
+		}
+	}
+
+	const is_2dimension_array_include = (array: number[][], item: number[]) => {
+		const mapped_array = array.map((a) => a[0].toString() + a[1].toString())
+		const mapped_item = item[0].toString() + item[1].toString()
+		return mapped_array.includes(mapped_item) 
 	}
 
 	return(
@@ -82,6 +279,9 @@ const Goban = () => {
 					)
 				})
 			))}
+			<button onClick={() => initializeGoban()}>
+				初期化する
+			</button>
 		</div>
 	)
 }
